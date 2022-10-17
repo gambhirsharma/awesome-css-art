c7363aa26798926fddbc25bd079d0abed56375e4
Gambhir <gambhir786191ss@gmail.com>
2022-10-17 12:53:52 +0530

    Cube background pattern added

---
+++ src/components/backgrounds/Cube.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './background.css'
+
+function Cube() {
+  return (
+    <div>
+      <div className="background-cube"></div>
+    </div>
+  )
+}
+
+export default Cube
