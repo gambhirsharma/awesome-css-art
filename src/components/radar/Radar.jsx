97dea9ba8e91a4319a36db7a5f37f1f3b2001c23
Gambhir <gambhir786191ss@gmail.com>
2022-10-18 13:39:22 +0530

    Radar cssmade some changes

--- src/components/radar/Radar.jsx
+++ src/components/radar/Radar.jsx
@@ -1,31 +1,31 @@
-import React from 'react'
-import "./radar.css"
+import React from "react";
+import "./radar.css";
 function Radar() {
   return (
     <div>
       <div className="canvas">
-  <div className="outer-circle">
-    <i></i>
-    <i></i>
-    <i></i>
-    <i></i>
-    <i></i>
-    <i></i>
-    <i></i>
-    <i></i>
-    <div className="radar"></div>
-    <div className="main-circle">
-      <p></p>
-      <p></p>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
+        <div className="outer-circle">
+          <i></i>
+          <i></i>
+          <i></i>
+          <i></i>
+          <i></i>
+          <i></i>
+          <i></i>
+          <i></i>
+          <div className="radar"></div>
+          <div className="main-circle">
+            <p></p>
+            <p></p>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+        </div>
+      </div>
     </div>
-  </div>
-</div>
-</div>
-  )
+  );
 }
 
-export default Radar
+export default Radar;
